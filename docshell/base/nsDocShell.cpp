83e6f0114e6c4d4b72e88149c434e19dab793131
Andrey Lushnikov <lushnikov@chromium.org>
2020-08-06 10:21:08 -0700

    finalize rebaseline

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -11821,7 +11821,6 @@ nsresult nsDocShell::OnLinkClick(
     target = aTargetSpec;
   }
 
-<<<<<<< HEAD
   RefPtr<nsDocShellLoadState> loadState = new nsDocShellLoadState(aURI);
   loadState->SetTarget(target);
   loadState->SetFileName(aFileName);
@@ -11836,19 +11835,8 @@ nsresult nsDocShell::OnLinkClick(
   nsCOMPtr<nsIRunnable> ev =
       new OnLinkClickEvent(this, aContent, loadState, noOpenerImplied,
                            aIsTrusted, aTriggeringPrincipal);
-||||||| parent of ca3ce9fca3e8... chore: bootstrap build #1153
-  nsCOMPtr<nsIRunnable> ev = new OnLinkClickEvent(
-      this, aContent, aURI, target, aFileName, aPostDataStream,
-      aHeadersDataStream, noOpenerImplied, aIsUserTriggered, aIsTrusted,
-      aTriggeringPrincipal, aCsp);
-=======
-  nsCOMPtr<nsIRunnable> ev = new OnLinkClickEvent(
-      this, aContent, aURI, target, aFileName, aPostDataStream,
-      aHeadersDataStream, noOpenerImplied, aIsUserTriggered, aIsTrusted,
-      aTriggeringPrincipal, aCsp);
   nsCOMPtr<nsIObserverService> observerService = mozilla::services::GetObserverService();
   observerService->NotifyObservers(ToSupports(aContent), "juggler-link-click", nullptr);
->>>>>>> ca3ce9fca3e8... chore: bootstrap build #1153
   return Dispatch(TaskCategory::UI, ev.forget());
 }
 
--- dom/workers/RuntimeService.cpp
+++ dom/workers/RuntimeService.cpp
@@ -1939,18 +1939,12 @@ void RuntimeService::PropagateStorageAccessPermissionGranted(
   }
 }
 
-<<<<<<< HEAD
-void RuntimeService::NoteIdleThread(SafeRefPtr<WorkerThread> aThread) {
-||||||| parent of ca3ce9fca3e8... chore: bootstrap build #1153
-void RuntimeService::NoteIdleThread(WorkerThread* aThread) {
-=======
 void RuntimeService::ResetDefaultLocaleInAllWorkers() {
   AssertIsOnMainThread();
   BROADCAST_ALL_WORKERS(ResetDefaultLocale);
 }
 
-void RuntimeService::NoteIdleThread(WorkerThread* aThread) {
->>>>>>> ca3ce9fca3e8... chore: bootstrap build #1153
+void RuntimeService::NoteIdleThread(SafeRefPtr<WorkerThread> aThread) {
   AssertIsOnMainThread();
   MOZ_ASSERT(aThread);
 
