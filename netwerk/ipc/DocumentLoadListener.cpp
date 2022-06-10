9736b11362995773b56c940a60db3641ca5123d4
Andrey Lushnikov <lushnikov@chromium.org>
2022-06-11 00:15:56 +0300

    fix compilation

--- netwerk/ipc/DocumentLoadListener.cpp
+++ netwerk/ipc/DocumentLoadListener.cpp
@@ -2441,7 +2441,7 @@ DocumentLoadListener::OnStartRequest(nsIRequest* aRequest) {
     nsCOMPtr<nsIObserverService> observerService =
         mozilla::services::GetObserverService();
     if (observerService) {
-      if (status === NS_OK)
+      if (status == NS_OK)
         observerService->NotifyObservers(ToSupports(loadingContext), "juggler-navigation-committed", NS_ConvertASCIItoUTF16(GetChannelCreationURI()->GetSpecOrDefault()).get());
       else
         observerService->NotifyObservers(ToSupports(loadingContext), "juggler-navigation-aborted", NS_ConvertASCIItoUTF16(nsPrintfCString("%u", status)).get());
