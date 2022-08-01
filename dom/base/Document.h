ab59a58a15fa2f803c1a3944454ca564b65b3019
playwright-devops <devops@playwright.dev>
2022-08-01 20:54:23 +0300

    conflicts

--- dom/base/Document.h
+++ dom/base/Document.h
@@ -4016,6 +4016,9 @@ class Document : public nsINode,
   // color-scheme meta tag.
   ColorScheme DefaultColorScheme() const;
 
+  bool PrefersReducedMotion() const;
+  bool ForcedColors() const;
+
   static bool HasRecentlyStartedForegroundLoads();
 
   static bool AutomaticStorageAccessPermissionCanBeGranted(
