268833748213d9e73fc358d806781773af2027f3
playwright-devops <devops@playwright.dev>
2023-07-28 06:57:41 -0700

    chore(ff): bootstrap build #1421

--- intl/components/src/TimeZone.h
+++ intl/components/src/TimeZone.h
@@ -155,6 +155,8 @@ class TimeZone final {
     return FillBufferWithICUCall(aBuffer, ucal_getHostTimeZone);
   }
 
+  static bool IsValidTimeZoneId(const char* timeZoneId);
+
   /**
    * Set the default time zone.
    */
